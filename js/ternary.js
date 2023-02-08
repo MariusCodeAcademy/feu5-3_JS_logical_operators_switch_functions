d23739b5b3bba95a97eda5c18d0e1fef992916e6
Marius CA <marius@example.com>
2023-02-08 12:58:41 +0200

    salys

--- js/ternary.js
+++ js/ternary.js
@@ -35,3 +35,23 @@ let timeOfDay =
     : 'labas vakaras';
 
 // console.log('timeOfDay ===', timeOfDay);
+
+let userName1 = 'James';
+let gender1 = 'male';
+let hasDrivigLicence1 = true;
+let ownsHouse1 = false;
+
+// Mr James drives car. He does not own a house.
+let title1 = gender1 === 'male' ? 'Mr' : 'Ms';
+let heOrShe1 = gender1 === 'female' ? 'She' : 'He';
+let houseOwning1 = ownsHouse1 === true ? 'owns' : 'does not own';
+// parasyti kad priklausomai nuo hasDrivigLicence1 butu 'drives' arba 'does not drive'
+let result1 = `${title1} ${userName1} drives car. ${heOrShe1} ${houseOwning1} a house.`;
+
+console.log('result1 ===', result1);
+
+let userName2 = 'Jane';
+let gender2 = 'female';
+let hasDrivigLicence2 = true;
+let ownsHouse2 = true;
+// Ms Jane does not drive car. She owns a house.
--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@
 ## switch
 
 1. parasyti programa kuri gauna viena skaiciu nuo 1 iki 5. ir priklausomai nuo skaiciau grazina kelintadienis yra lietuviskai. jei 1 tai pirmadienis, 2 antradienis ir t.t. (&& || cia nereikia)
+
+2. sukuriam kintamaji salis ir prilyginam vienam is europos saliu. Sukuriam palyginimo programa kuri pranestu:  
+   2.1 BALTIJOS SALYS - jei salis yra Lietuva, Estija arba Latvija
+   2.2 Vakaru europa - jei salis yra UK, Prancuzija, Vokietija
+   3.3. Kitos Salys - jei salis yra neaprasyta pirmais 2 punktais
