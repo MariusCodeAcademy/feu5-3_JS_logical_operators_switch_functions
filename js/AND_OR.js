feaab385ddd90338a30c9d31abd666a328ebf350
Marius CA <marius@example.com>
2023-02-08 10:34:40 +0200

    logical table

---
+++ js/AND_OR.js
@@ -0,0 +1,31 @@
+'use strict';
+console.log('AND_OR.js file was loaded');
+
+let n1 = '8';
+
+// parasyti ar n1 yra skaiciaus tipo IR teigiamas?
+// nested if
+if (typeof n1 === 'number') {
+  // n1 yra sk tipo
+  if (n1 > 0) {
+    // n1 yra skaicaus tipo ir daugiau uz 0
+  }
+}
+// logical operator
+if (typeof n1 === 'number' && n1 > 0) {
+  // n1 yra skaicaus tipo ir daugiau uz 0
+  console.log('n1 yra skaicaus tipo ir daugiau uz 0');
+}
+
+// lentele
+// AND - lietuviskai IR
+console.log(`true && true === ${true && true}`);
+console.log(`true && false === ${true && false}`);
+console.log(`false && true === ${false && true}`);
+console.log(`false && false === ${false && false}`);
+
+// OR - lietuviskai ARBA
+console.log('true || true ===', true || true);
+console.log('true || false ===', true || false);
+console.log('false || true ===', false || true);
+console.log('false || false ===', false || false);
